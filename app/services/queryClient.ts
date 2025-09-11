651ae05869c30722f0784e7bb9baa7670022e094
Evans Gitahi <evanygk@gmail.com>
2025-09-11 06:52:27 +0300

    setup a service layer and fetch some movies

---
+++ app/services/queryClient.ts
@@ -0,0 +1,22 @@
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const ACCESS_TOKEN = process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+
+async function fetchFromAPI(endpoint: string) {
+    const res = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}`, {
+        headers: {
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+            Accept: "application/json",
+        }
+    });
+    if (!res.ok) throw new Error("Failed to fetch data");
+    return res.json();
+}
+
+export const QueryService = {
+    getDailyTrending: () => fetchFromAPI("/trending/movie/day"),
+    getWeeklyTrending: () => fetchFromAPI("/trending/movie/week"),
+    getMovieDetails: (id: number) => fetchFromAPI(`/movie/${id}`),
+    searchMovies: (query: string) => fetchFromAPI(`/search/movie&query=${query}`),
+}

49dd9cdb7a3543c87787239800c5e9056d7b106f
Evans Gitahi <evanygk@gmail.com>
2025-09-17 10:30:35 +0300

    change to toggle icon for theme

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { Sun, Moon } from "lucide-react"; // install: npm i lucide-react
+import { Sun, Moon, ToggleLeft, ToggleRight } from "lucide-react"; // install: npm i lucide-react
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -16,7 +16,7 @@ export default function ThemeToggle() {
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 transition"
     >
-      {theme === "light" ? <Moon size={20} /> : <Sun size={20} color={"#FFD700"} />}
+      {theme === "light" ? <ToggleLeft size={20} /> : <ToggleRight size={20} color={"#FFD700"} />}
     </button>
   );
 }
